278803fcb9e88c889761c3987fe3ed973d30b36f
Daniel M <dmunch@lucca.fr>
2016-01-09 22:08:43 +0100

    Update berlin.sh
    Add new link to up-to-date GTFS file for Berlin.

--- docker-gtfsdb/berlin.sh
+++ docker-gtfsdb/berlin.sh
@@ -1,2 +1,3 @@
-docker-compose run gtfsdb http://www.vbb.de/de/datei/GTFS_VBB_Sep2015_Dez2015.zip
+docker-compose run gtfsdb http://www.vbb.de/de/download/GTFS_VBB_Dez2015_Dez2016.zip
+#docker-compose run gtfsdb http://www.vbb.de/de/datei/GTFS_VBB_Sep2015_Dez2015.zip
 #docker run -i -t --rm dmunch/gtfsdb --database_url postgresql://docker:docker@192.168.99.100:25432/gis http://www.vbb.de/de/datei/GTFS_VBB_Sep2015_Dez2015.zip --is_geospatial
